2ab62d6041d92dd7df155346348394c275bffc90
Chris Henry <cdhenry@gmail.com>
2018-04-27 20:19:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ function createIssue() {
 }
 
 function showResults(json) {
+
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST'
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '4e07e51952b11d82e390edef738ceee19457c2d9'
 }
